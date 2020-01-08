fa45beb00f53c0d85f6653605de3dc40ac598a59
Godsend Joseph <bobbyjoseph108@gmail.com>
2020-01-08 19:37:55 +0100

    edited the UI in the next screen

--- lib/screen/newScreen.dart
+++ lib/screen/newScreen.dart
@@ -6,26 +6,32 @@ class NewScreen extends StatefulWidget {
 }
 
 class _NewScreenState extends State<NewScreen> {
+  Widget _getList() {
+    var listV = ListView(
+      children: <Widget>[
+        Card(
+          margin: EdgeInsets.only(left: 15.0, right: 15.0, top: 10.0),
+          elevation: 10.0,
+          child: ListTile(
+            leading: Icon(Icons.home),
+            title: Text("Home"),
+            subtitle: Text("Welcome Home"),
+            onTap: () {},
+          ),
+        )
+      ],
+    );
+
+    return listV;
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Profile Screen"),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: <Widget>[
-          Center(
-            child: Text(
-              "Hello There",
-              style: TextStyle(
-                fontFamily: 'serif',
-                fontSize: 30.0,
-              ),
-              ),
-          ),
-        ],
-      ),
+      body: _getList(),
     );
   }
 }
